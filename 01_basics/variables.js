f2c91bc52f0fd60829a81ad8d70b2d48d191f237
Shravan Carpenter <113225324+ShravanCarpenter@users.noreply.github.com>
2024-07-04 07:15:14 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 211054
+let accountEmail = "shravan22@gmail.com"
+var accountPwd = "12345"
+accountCity = "Hatpipliya"
+
+//accountId = 2
+
+/* var is not used because of the issue 
+in block scope and functional scope */
+
+accountCity = "Indore"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPwd, accountCity])
