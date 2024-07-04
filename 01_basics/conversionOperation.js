a3c6bbc331994880f8bed58d25cf1e8d53f2609a
Shravan Carpenter <113225324+ShravanCarpenter@users.noreply.github.com>
2024-07-04 07:35:45 +0000

    conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,15 @@
+let score = null
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+
+//"33" = 33
+//"33abs" = NaN
+//true = 1, false = 0
+//null = 0
+//"" = false
+//"ram" = true
